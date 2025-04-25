68cd463b640df339c3e9e99111a6fcf7926f2ee7
wsp <807569852@qq.com>
2025-04-26 02:16:00 +0800

    1

--- implementation/programs_database.py
+++ implementation/programs_database.py
@@ -58,8 +58,7 @@ def _reduce_score(scores_per_test: ScoresPerTest) -> float:
     """
     # TODO RZ: change the code to average the score of each test.
     # return scores_per_test[list(scores_per_test.keys())[-1]]
-    test_scores = [scores_per_test[k] for k in scores_per_test.keys()]
-    return sum(test_scores) / len(test_scores)
+    return scores_per_test["composite"]
 
 
 def _get_signature(scores_per_test: ScoresPerTest) -> Signature:
